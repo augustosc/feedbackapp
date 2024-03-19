7ea7e34eabb906b322f424718b77de791c0a73b1
Augusto Cavalcanti <augusto.sherman@gmail.com>
2024-03-19 20:35:54 -0300

    change title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,7 +26,7 @@ function Header({text,bgColor,textColor}) {
 }
 
 Header.defaultProps = {
-    text: 'Feedback UI',
+    text: 'Feedback Review UI',
     bgColor: 'rgba(0,0,0,0.4)',
     textColor: '#ff6a95'
 }
